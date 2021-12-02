5ab60df72d70e05282eb7715c4876354a224c791
Diego Gonzalez <45858@psdschool.org>
2021-12-02 10:49:15 -0700

    Added new screen

---
+++ Made_In_Heaven/FirstScreen.swift
@@ -0,0 +1,29 @@
+//
+//  FirstScreen.swift
+//  Made_In_Heaven
+//
+//  Created by Diego Gonzalez on 12/2/21.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
